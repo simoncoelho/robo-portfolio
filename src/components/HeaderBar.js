e313a4219eef82c526cf6ef49abfe4d38873fcd8
Simon <coelhosimon@hotmail.com>
2025-02-23 18:51:30 -0700

    Header bar changes

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,11 +1,32 @@
 // HeaderBar.jsx
 import React from "react";
-import "./HeaderBar.css"; // the same CSS from above or inline styles
+import { FaLinkedin, fax } from "react-icons/fa";
+import "./HeaderBar.css"; // We'll define basic styling below
+import { TbBrandX } from "react-icons/tb";
 
 export default function HeaderBar() {
   return (
     <header className="header-bar">
       <div className="logo">simon coelho</div>
+      <div className="social-links" style={{ justify: "right" }}>
+        {/* LinkedIn icon -> wrap in an <a> tag linking to your profile */}
+        <a
+          href="https://www.linkedin.com/in/simon-coelho"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin size={34} color="white"/>
+        </a>
+
+        {/* Twitter icon -> likewise, link to your Twitter */}
+        <a
+          href="https://x.com/simon_coelho"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <TbBrandX size={34} color="white"/>
+        </a>
+      </div>
     </header>
   );
 }
